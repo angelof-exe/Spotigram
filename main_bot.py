5018d084021557e81f007033e13c6055e64f3632
Angelo Franciamore <angelo99franciamore@gmail.com>
2022-10-28 10:26:10 +0200

    feat: search the infos of an album (only italian for now)
    fix: enhancement of the file opening for text language

--- main_bot.py
+++ main_bot.py
@@ -1,64 +1,65 @@
+from ast import parse
 from setting import bot, sp, user_language
 # import bot_en
 # import bot_it
 
 # Set the language for the bot
-global start_txt
+global start_txt #For /start
 start_txt = ''
-global top_track_txt
+global top_track_txt #For /top
 top_track_txt = ''
-global change_lang
-change_lang = ''
+global change_lang_txt #When you use /start send a messager if you want change the language
+change_lang_txt = ''
+global info_album_txt
+info_album_txt = ''
 
 def check_and_set_language():
     global start_txt
     global top_track_txt
+    global change_lang_txt
+    global info_album_txt
 
     if(user_language == "it"):
         lan = "txt_it"
-
-        f = open(f"{lan}/welcome.txt", "r")
-
-        start_txt = str(f.read())
-        f.close()
-
-        f = open(f"{lan}/top_track.txt", "r")
-        top_track_txt = str(f.read())
-        f.close()
-
-        f = open(f"{lan}/change_lang.txt", "r")
-        change_lang = str(f.read())
-        f.close()
-
     elif((user_language == "en") or (user_language.lower() == "none")):
         lan = "txt_en"
 
-        f = open(f"{lan}/welcome.txt", "r")
-        start_txt = str(f.read())
-        f.close()
+    f = open(f"{lan}/welcome.txt", "r")
+    start_txt = str(f.read())
+    f.close()
 
-        f = open(f"{lan}/top_track.txt", "r")
-        top_track_txt = str(f.read())
-        f.close()
+    f = open(f"{lan}/top_track.txt", "r")
+    top_track_txt = str(f.read())
+    f.close()
 
-        f = open(f"{lan}/change_lang.txt", "r")
-        change_lang = str(f.read())
-        f.close()
+    f = open(f"{lan}/change_lang.txt", "r")
+    change_lang_txt = str(f.read())
+    f.close()
+
+    f = open(f"{lan}/info_album.txt", "r")
+    info_album_txt = str(f.read())
+    f.close()
 
 
 @bot.message_handler(commands=['start'])
-def send_start(message):
+def bot_send_start(message):
     check_and_set_language()
     chat_id = message.chat.id
 
-    bot.send_message(chat_id, start_txt)
-    bot.send_message(chat_id, change_lang)
+    bot.send_message(chat_id, start_txt, parse_mode= "markdown")
+    bot.send_message(chat_id, change_lang_txt)
 
 @bot.message_handler(commands=['top'])
-def top_tracks(message):
+def bot_top_tracks(message):
     chat_id = message.chat.id
     bot.send_message(chat_id, top_track_txt)
     bot.register_next_step_handler(message, top_track)
+
+@bot.message_handler(commands=['infoalbum'])
+def bot_info_album_txt(message):
+    chat_id = message.chat.id
+    bot.send_message(chat_id, info_album_txt)
+    bot.register_next_step_handler(message, info_album)
 
 # ================== TOP 5 TRACKS ==================
 def top_track(message):
@@ -76,16 +77,47 @@ def top_track(message):
         buffer += str(idx + 1) + " - " + track['name'] + "\n"
 
     bot.send_message(chat_id_tmp, buffer)
+# ======================================================
 
+# ================== INFO ALBUM ==================
+def info_album(message):
+    try:
+       chat_id = message.chat.id
+       input_text = message.text
+       album_id = search_album_id(str(input_text))
+       results = sp.album(album_id)
+    except Exception as e:
+       bot.send_message(chat_id, "There was an error!")
+       raise e("Invalid text")
 
-#Return the URI (a sort of ID) of the artist
+    album_result = results
+
+    #Caption to send the message
+    name_album_txt = "Nome dell'album: " + str(album_result['name'])
+    total_tracks_txt = "Numero di tracce totali: " + str(album_result['total_tracks'])
+    release_data_txt = "Data di rilascio: " + str(album_result['release_date'])
+    caption = name_album_txt + "\n" + total_tracks_txt + "\n" + release_data_txt
+
+    image_url = album_result['images'][0]['url']
+
+    bot.send_photo(chat_id, image_url, caption)
+# ======================================================
+
+# Return the URI (a sort of ID) of the artist
 def search_artist_id(artist_name) -> str:
     results = sp.search(artist_name, limit= 1, type="artist")
     items = results['artists']['items']
     artist_item = items[0]
     return(str(artist_item['uri']))
-# ======================================================
 
+# Return the URI of the album
+def search_album_id(nome_album) -> str:
+    results = sp.search(nome_album, limit= 1, type="album")
+    items = results['albums']['items']
+    album_item = items[0]
+    return(str(album_item['uri']))
+
+# Bot commands for change the language
 
 # Change the language to italian
 @bot.message_handler(commands=['ita'])
